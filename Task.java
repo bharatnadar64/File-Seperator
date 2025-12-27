43182061676139c49095aca89fa4c1c9986e084b
beatjoy15 <52093358+beatjoy15@users.noreply.github.com>
2025-12-27 12:13:44 +0530

    Updated Task.java

--- Task.java
+++ Task.java
@@ -1,3 +1,5 @@
+import java.io.FileWriter;
+import java.io.IOException;
 import java.util.HashMap;
 import java.util.Map;
 
@@ -12,11 +14,31 @@ public class Task {
         ext.put("docx", 30);
         ext.put("mp4", 12);
 
-        // Print the map
-        for (Map.Entry<String, Integer> entry : ext.entrySet()) {
-            System.out.println(entry.getKey() + " -> " + entry.getValue());
-        }
+     //  Print the map
+     //   for (Map.Entry<String, Integer> entry : ext.entrySet()) {
+     //       System.out.println(entry.getKey() + " -> " + entry.getValue());
+     //    }
 
       // create logic to add these values in excel/csv file.
+         String filePath = "file_extensions.csv";
+
+        try (FileWriter writer = new FileWriter(filePath)) {
+
+            // Header
+            writer.append("Extension,Count\n");
+
+            // Data
+            for (Map.Entry<String, Integer> entry : ext.entrySet()) {
+                writer.append(entry.getKey())
+                      .append(",")
+                      .append(String.valueOf(entry.getValue()))
+                      .append("\n");
+            }
+
+            System.out.println("CSV file created successfully!");
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
     }
 }
